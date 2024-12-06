617abd00ce6639e89573dc6257a8fca4c713f39c
ahreum <126214624+aaahreum@users.noreply.github.com>
2024-12-06 20:39:57 +0900

    [level 1] Title: 콜라 문제, Time: 0.09 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while (n >= a) {
+            int x = (n / a) * b; 
+            answer += x;        
+            n = x + (n % a);    
+        }
+        
+        return answer;
+    }
+}
