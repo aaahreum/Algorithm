15609db5a6355cfe0030313ce013506ec6bbba24
ahreum <126214624+aaahreum@users.noreply.github.com>
2024-12-11 10:07:43 +0900

    [level 1] Title: 모의고사, Time: 0.60 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public List<Integer> solution(int[] answers) {
+        int[] number1 = {1, 2, 3, 4, 5};
+        int[] number2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] number3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = new int[3];
+        
+        for(int i = 0; i < answers.length ; i++){
+            if(answers[i] == number1[i % 5]){
+                score[0] ++;
+            }
+            if (answers[i] == number2[i % 8]){
+                score[1] ++;
+            }
+            if (answers[i] == number3[i % 10]){
+                score[2] ++;
+            }
+        }
+        
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < 3; i++) {
+            if (maxScore == score[i]) list.add(i + 1);
+        }
+        
+        return list;
+    }
+}
